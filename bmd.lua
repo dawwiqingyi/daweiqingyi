5645b5b192001f71392e84291fa6b95191642613
dawwiqingyi <749204033@qq.com>
2025-09-12 16:01:30 +0800

    Update bmd.lua

--- bmd.lua
+++ bmd.lua
@@ -1,17 +1,16 @@
--- 白名单配置文件 - 仅用户名验证 (Lua格式)
-return {
-    version = "1.0",
-    description = "白名单配置文件 - 仅用户名验证",
-    updated = "2024-01-15",
-    usernames = {
-        ["weidada5"] = true,
-        ["得不到就毁受想要也只能干望着"] = true,
-        ["ExampleUser1"] = true,
-        ["ExampleUser2"] = true
-    },
-    settings = {
-        strict_mode = false,
-        allow_display_name = true,
-        case_sensitive = true
-    }
+{
+  "version": "1.0",
+  "description": "白名单配置文件 - 仅用户名验证",
+  "updated": "2024-01-15",
+  "usernames": {
+    "weidada5": true,
+    "得不到就毁受想要也只能干望着": true,
+    "ExampleUser1": true,
+    "ExampleUser2": true
+  },
+  "settings": {
+    "strict_mode": false,
+    "allow_display_name": true,
+    "case_sensitive": true
+  }
 }
