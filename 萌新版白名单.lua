203d8be87b3ff74c509f4dd5e26c660a97f357e0
dawwiqingyi <749204033@qq.com>
2025-11-06 20:44:37 +0800

    Create 萌新版白名单.lua

---
+++ 萌新版白名单.lua
@@ -0,0 +1,18 @@
+{
+  "version": "1.0",
+  "description": "萌新版白名单配置文件 - 用户名验证",
+  "updated": "2025-11-06",
+  "usernames": {
+   
+    "91xfh78": true
+    
+    
+    
+    
+  },
+  "settings": {
+    "strict_mode": false,
+    "allow_display_name": true,
+    "case_sensitive": true
+  }
+}
