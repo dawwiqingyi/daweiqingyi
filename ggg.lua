a92977ebf49a06ed7f19d0d801c6831397c712d0
dawwiqingyi <749204033@qq.com>
2026-02-01 15:58:34 +0800

    Update ggg.lua

--- ggg.lua
+++ ggg.lua
@@ -7,3 +7,57 @@ pcall(function()
         Duration = 8
     })
 end)
+local StarterGui = game:GetService("StarterGui")
+
+pcall(function()
+    StarterGui:SetCore("SendNotification", {
+        Title = "吃吃世界Pro 公告",
+        Text = "当前版本已内置霸凌总榜、自动升级等功能。\n如有问题请联系作者更新。",
+        Duration = 8
+    })
+end)
+local StarterGui = game:GetService("StarterGui")
+
+pcall(function()
+    StarterGui:SetCore("SendNotification", {
+        Title = "吃吃世界Pro 公告",
+        Text = "当前版本已内置霸凌总榜、自动升级等功能。\n如有问题请联系作者更新。",
+        Duration = 8
+    })
+end)
+local StarterGui = game:GetService("StarterGui")
+
+pcall(function()
+    StarterGui:SetCore("SendNotification", {
+        Title = "吃吃世界Pro 公告",
+        Text = "当前版本已内置霸凌总榜、自动升级等功能。\n如有问题请联系作者更新。",
+        Duration = 8
+    })
+end)
+local StarterGui = game:GetService("StarterGui")
+
+pcall(function()
+    StarterGui:SetCore("SendNotification", {
+        Title = "吃吃世界Pro 公告",
+        Text = "当前版本已内置霸凌总榜、自动升级等功能。\n如有问题请联系作者更新。",
+        Duration = 8
+    })
+end)
+local StarterGui = game:GetService("StarterGui")
+
+pcall(function()
+    StarterGui:SetCore("SendNotification", {
+        Title = "吃吃世界Pro 公告",
+        Text = "当前版本已内置霸凌总榜、自动升级等功能。\n如有问题请联系作者更新。",
+        Duration = 8
+    })
+end)
+local StarterGui = game:GetService("StarterGui")
+
+pcall(function()
+    StarterGui:SetCore("SendNotification", {
+        Title = "吃吃世界Pro 公告",
+        Text = "当前版本已内置霸凌总榜、自动升级等功能。\n如有问题请联系作者更新。",
+        Duration = 8
+    })
+end)
